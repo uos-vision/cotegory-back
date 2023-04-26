0665146acc044be8a12a593011ebfda47420cb08
hyeongcheolkim <tori1753@naver.com>
2023-04-26 10:59:54 +0900

    feat: Admin controller 추가. 및 @PreAuthorize 작동 확인

---
+++ src/main/java/vision/cotegory/controller/AdminRestController.java
@@ -0,0 +1,33 @@
+package vision.cotegory.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vision.cotegory.controller.response.CurrentIdResponse;
+import vision.cotegory.repository.MemberRepository;
+import vision.cotegory.security.JWTUtil;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin")
+@Transactional
+public class AdminRestController {
+
+    private final JWTUtil jwtUtil;
+    private final MemberRepository memberRepository;
+
+    @PreAuthorize(value = "hasRole('ROLE_ADMIN')")
+    @Operation(description = "테스트용도로만 사용하세요")
+    @GetMapping("/current-id")
+    public ResponseEntity<CurrentIdResponse> currentId(@RequestHeader(value = "Authorization") String jwtKey) {
+        return ResponseEntity.ok(CurrentIdResponse.builder()
+                .currentId(String.valueOf(jwtUtil.getMemberId(jwtKey)))
+                .build());
+    }
+}
