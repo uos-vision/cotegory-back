cb2ec405369eeebc56ae5c81dc00778bdcca61d5
hyeongcheolkim <tori1753@naver.com>
2023-04-26 11:32:04 +0900

    refactor: import 정리

--- src/main/java/vision/cotegory/config/SwaggerConfig.java
+++ src/main/java/vision/cotegory/config/SwaggerConfig.java
@@ -1,7 +1,5 @@
 package vision.cotegory.config;
 
-import com.nimbusds.jwt.JWT;
-import io.swagger.models.Swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -9,7 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.HttpAuthenticationScheme;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
