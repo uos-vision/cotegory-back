e66a34713fb719d3803c303a8a92d2834ed99acf
hyeongcheolkim <tori1753@naver.com>
2023-04-26 11:31:27 +0900

    feat: Swagger jwt인증토큰 입력시, 맨 앞에 Bearer를 생략가능하게 변경

--- src/main/java/vision/cotegory/config/SwaggerConfig.java
+++ src/main/java/vision/cotegory/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package vision.cotegory.config;
 
+import com.nimbusds.jwt.JWT;
+import io.swagger.models.Swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -7,10 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -21,7 +20,7 @@ import java.util.List;
 public class SwaggerConfig {
 
     @Bean
-    public Docket api(){
+    public Docket api() {
         return new Docket(DocumentationType.OAS_30)
                 .ignoredParameterTypes(RequestHeader.class)
                 .useDefaultResponseMessages(false)
@@ -29,7 +28,7 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
-                .securitySchemes(List.of(apiKey()))
+                .securitySchemes(List.of(bearerAuthSecurityScheme()))
                 .securityContexts(List.of(securityContext()))
                 .apiInfo(apiInfo());
     }
@@ -46,8 +45,8 @@ public class SwaggerConfig {
         return List.of(new SecurityReference("Authorization", new AuthorizationScope[]{authorizationScope}));
     }
 
-    private ApiKey apiKey() {
-        return new ApiKey("Authorization", "Bearer Token", "header");
+    private HttpAuthenticationScheme bearerAuthSecurityScheme() {
+        return HttpAuthenticationScheme.JWT_BEARER_BUILDER.name("Authorization").build();
     }
 
     private ApiInfo apiInfo() {
