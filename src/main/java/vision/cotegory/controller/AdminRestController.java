cf0700ec0e416636cc94b4acff2c74a1802c8aa7
hyeongcheolkim <tori1753@naver.com>
2023-04-30 18:26:32 +0900

    refactor: import 정리

--- src/main/java/vision/cotegory/controller/AdminRestController.java
+++ src/main/java/vision/cotegory/controller/AdminRestController.java
@@ -2,17 +2,17 @@ package vision.cotegory.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import vision.cotegory.controller.request.CreateQuizRequest;
 import vision.cotegory.controller.request.DeactivateRequest;
 import vision.cotegory.controller.response.AbnormalQuizResponse;
-import vision.cotegory.controller.response.CurrentIdResponse;
 import vision.cotegory.entity.AbnormalQuiz;
 import vision.cotegory.entity.Origin;
 import vision.cotegory.entity.TagGroup;
@@ -34,7 +34,6 @@ import javax.validation.Valid;
 @PreAuthorize(value = "hasRole('ROLE_ADMIN')")
 public class AdminRestController {
 
-    private final JWTUtil jwtUtil;
     private final AbnormalQuizService abnormalQuizService;
     private final QuizService quizService;
     private final AbnormalQuizRepository abnormalQuizRepository;
