ee079b812174001a36ab9517bd069ccd703c4e9b
hyeongcheolkim <tori1753@naver.com>
2023-05-02 06:48:42 +0900

    refactor: 메서드 최적화

--- src/main/java/vision/cotegory/repository/MemberRepository.java
+++ src/main/java/vision/cotegory/repository/MemberRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByLoginId(String loginId);
-    Optional<Member> findByLoginIdAndActivated(String loginId, Boolean activated);
+    Optional<Member> findByLoginIdAndActivatedIsTrue(String loginId);
 
     @Query("select m.id from Member m where m.activated = TRUE and m.loginId = :loginId")
     Optional<Long> getIdByLoginId(@Param("loginId") String loginId);
