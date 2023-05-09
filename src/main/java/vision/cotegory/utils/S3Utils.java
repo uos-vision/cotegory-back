8d735740c31bb36297923a2876025132e27ad5fd
hyeongcheolkim <tori1753@naver.com>
2023-05-09 15:14:27 +0900

    refactor: 사용하지 않는 코드 제거

--- src/main/java/vision/cotegory/utils/S3Utils.java
+++ src/main/java/vision/cotegory/utils/S3Utils.java
@@ -2,14 +2,12 @@ package vision.cotegory.utils;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3URI;
-import com.amazonaws.services.s3.model.DeleteObjectsResult;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-import vision.cotegory.exception.exception.BusinessException;
 import vision.cotegory.exception.exception.S3Exception;
 
 import java.io.IOException;
