92574d51aedeb9dad0a5053d6993b38083710396
hyeongcheolkim <tori1753@naver.com>
2023-05-26 06:09:13 +0900

    feat: 누락된 annotation 추가

--- src/main/java/vision/cotegory/controller/request/ChangeBaekjoonHandleRequest.java
+++ src/main/java/vision/cotegory/controller/request/ChangeBaekjoonHandleRequest.java
@@ -2,7 +2,10 @@ package vision.cotegory.controller.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class ChangeBaekjoonHandleRequest {
+    @NotBlank
     private String baekjoonHandle;
 }
--- src/main/java/vision/cotegory/controller/request/CreateProblemMetaRequest.java
+++ src/main/java/vision/cotegory/controller/request/CreateProblemMetaRequest.java
@@ -3,15 +3,23 @@ package vision.cotegory.controller.request;
 import lombok.Data;
 import vision.cotegory.entity.Origin;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class CreateProblemMetaRequest {
+    @NotNull
     private Integer problemNumber;
 
+    @NotNull
     private Origin origin;
 
+    @NotBlank
     private String title;
 
+    @NotBlank
     private String url;
 
+    @NotNull
     private Boolean isCompany;
 }
