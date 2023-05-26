6aa6d273f09fcf8821cde67e9e179db050913412
hyeongcheolkim <tori1753@naver.com>
2023-05-26 22:15:34 +0900

    feat: Ai서버와 통신 실패시 일정간격으로(200millis) 3번까지 재시도하는 로직 추가

--- src/main/java/vision/cotegory/problemloader/ai/AiWebClient.java
+++ src/main/java/vision/cotegory/problemloader/ai/AiWebClient.java
@@ -6,8 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 import vision.cotegory.exception.exception.AiApiException;
 
+import java.time.Duration;
 import java.util.List;
 
 @Component
@@ -35,6 +37,12 @@ public class AiWebClient {
                     return Mono.error(new AiApiException());
                 })
                 .bodyToFlux(Integer.class)
+                .retryWhen(Retry.fixedDelay(3, Duration.ofMillis(200))
+                        .filter(throwable -> throwable instanceof AiApiException)
+                        .doBeforeRetry(before -> log.info("[aiConnect] retry"))
+                        .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> {
+                            throw new AiApiException(retrySignal.toString());
+                        }))
                 .collectList()
                 .block();
     }
